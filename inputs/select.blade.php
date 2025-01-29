67e83257d3cac5a5f248b7b4f0c9b20247d7313b
Pedro Tebexreni <pedro.tebexreni@estudiono.com>
2025-01-29 11:31:58 -0300

    Remove duplicated classes in select component

--- inputs/select.blade.php
+++ inputs/select.blade.php
@@ -1,4 +1,4 @@
-@props(['options', 'selected' => null, 'placeholder' => 'Selecione uma opção', 'name' => 'select-name', 'variation' => 'default'])
+@props(['options', 'selected' => null, 'placeholder' => 'Selecione uma opção', 'name' => 'select-name', 'variation' => 'default', 'open' => false,])
 
 <div x-data="{ open: false, selected: '{{ $selected }}', placeholder: '{{ $placeholder }}' }" @click.away="open = false" class="relative group">
     <!-- Default -->
@@ -25,30 +25,25 @@
 
     <!-- WW Variation -->
     @elseif ($variation === 'ww')
-        <div @click="open = !open"
-            class="font-raleway font-[400] text-[12px] text-white dark:text-white px-sm h-lg w-full flex items-center cursor-pointer"
-            :class="{ 
-                'bg-black-2 rounded-t-sm': open,  <!-- black-2 background when open -->
-                'rounded-sm bg-black-2 border-transparent': !open <!-- black-2 background when closed -->
-            }">
-            <span x-text="selected ? selected : placeholder" class="text-xs"></span>
-            <div class="absolute top-0 right-0 flex items-center h-lg mr-sm">
-                <span class="material-symbols-outlined !text-[12px] !leading-[125%] text-white" x-show="!open">expand_more</span>
-                <span class="material-symbols-outlined !text-[12px] !leading-[125%] text-white" x-show="open">expand_less</span>
-            </div>
+    <div @click="open = !open"
+    class="bg-black-2 font-raleway font-[400] text-[12px] text-white dark:text-white px-sm h-lg w-full flex items-center cursor-pointer {{ $open ? 'rounded-sm' : 'rounded-t-sm' }}">
+        <span x-text="selected ? selected : placeholder" class="text-xs"></span>
+        <div class="absolute top-0 right-0 flex items-center h-lg mr-sm">
+            <span class="material-symbols-outlined !text-[12px] !leading-[125%] text-white" x-show="!open">expand_more</span>
+            <span class="material-symbols-outlined !text-[12px] !leading-[125%] text-white" x-show="open">expand_less</span>
         </div>
+    </div>
 
-        <div x-show="open" class="absolute z-50 w-full overflow-hidden border rounded-b-sm bg-black-2 border-transparent">
-            <ul class="overflow-auto max-h-60">
-                <template x-for="(option, index) in {{ json_encode($options) }}" :key="index">
-                    <li @click="selected = option; open = false" x-text="option"
-                        class="text-white flex font-raleway font-[400] text-[12px] px-sm h-lg cursor-pointer hover:bg-black-3 items-center">
-                    </li>
-                </template>
-            </ul>
-        </div>
-    @endif
-
+    <div x-show="open" class="absolute z-50 w-full overflow-hidden border rounded-b-sm bg-black-2 border-transparent">
+        <ul class="overflow-auto max-h-60">
+            <template x-for="(option, index) in {{ json_encode($options) }}" :key="index">
+                <li @click="selected = option; open = false" x-text="option"
+                    class="text-white flex font-raleway font-[400] text-[12px] px-sm h-lg cursor-pointer hover:bg-black-3 items-center">
+                </li>
+            </template>
+        </ul>
+    </div>
+@endif
     <!-- Hidden Input -->
     <input type="hidden" :value="selected" name="{{ $name }}">
 </div>
