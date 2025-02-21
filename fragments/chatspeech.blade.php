f267ea06d4224c5e7eab41a7ec1c0ef51f7b5a3f
Pedro Tebexreni <pedro.tebexreni@estudiono.com>
2025-02-21 14:45:56 -0300

    added lightbox for image display in chat speech

--- fragments/chatspeech.blade.php
+++ fragments/chatspeech.blade.php
@@ -1,14 +1,15 @@
 @props([
-    'avatarSrc', // Avatar image path
-    'name', // Sender name
-    'messages' => [], // Array of messages (content, sent_at, media_url, has_media)
-    'senderType', // 'client' or 'user'
+    'avatarSrc',        // URL of the avatar image
+    'name',              // Name of the user
+    'messages' => [],    // Array of messages to display
+    'senderType',        // Type of sender (user or client)
 ])
 
 @if($messages)
 <div class="flex gap-xxxs {{ $senderType === 'user' ? 'justify-end' : '' }}">
     @if($senderType === 'client')
     <div class="flex items-start">
+        <!-- Display client's avatar -->
         <x-shared.fragments.useravatar src="{{ $avatarSrc }}" size="sm" />
     </div>
     @endif
@@ -21,11 +22,14 @@
 
         @if (isset($message['has_media']) && $message['has_media'])
             @if (strpos($message['media_url'], '.mp4') !== false)
-            <div class="w-full max-w-[240px] min-h-xxxl relative">
-                <video id="video_{{ $loop->index }}" class="w-full h-full object-cover rounded-xxs hidden" controls>
+            <!-- Video message with Alpine.js control -->
+            <div x-data="{ showVideo: false }" class="w-full max-w-[240px] min-h-xxxl relative">
+                <!-- Video player, shown when video is clicked -->
+                <video x-show="showVideo" class="w-full h-full object-cover rounded-xxs" controls>
                     <source src="{{ $message['media_url'] }}" type="video/mp4">
                 </video>
-                <div id="overlay_{{ $loop->index }}" class="absolute inset-0 flex items-center justify-center bg-black/50 cursor-pointer rounded-xxs">
+                <!-- Play button overlay when video is not shown -->
+                <div x-show="!showVideo" @click="showVideo = true" class="absolute inset-0 flex items-center justify-center bg-black/50 cursor-pointer rounded-xxs">
                     <span class="material-symbols-outlined text-white text-6xl">play_arrow</span>
                 </div>
                 <div class="absolute bottom-0 right-0 p-xs">
@@ -33,14 +37,22 @@
                 </div>
             </div>
             @else
-            <div class="w-full max-w-[240px] min-h-xxxl relative">
-                <img class="w-full h-full object-cover rounded-xxs" src="{{ $message['media_url'] }}" alt="Message image">
+            <!-- Image message with Lightbox using Alpine.js -->
+            <div x-data="{ open: false }" class="w-full max-w-[240px] min-h-xxxl relative">
+                <!-- Image thumbnail that opens the lightbox on click -->
+                <img @click="open = true" class="w-full h-full object-cover rounded-xxs cursor-pointer" src="{{ $message['media_url'] }}" alt="Message image">
                 <div class="absolute bottom-0 right-0 p-xs">
                     <x-shared.typography.body size="sm" class="text-gray-3">{{ $formattedTime }}</x-shared.typography.body>
+                </div>
+
+                <!-- Lightbox modal when image is clicked -->
+                <div x-show="open" class="fixed inset-0 bg-black-1/90 flex items-center justify-center z-50" @click="open = false" x-transition>
+                    <img class="max-w-full max-h-full p-lg" src="{{ $message['media_url'] }}" alt="Full Image">
                 </div>
             </div>
             @endif
         @elseif ($message['content'])
+        <!-- Text message -->
         <div class="max-w-[240px] w-fit bg-black-1 flex flex-col p-xs rounded-xxs">
             <x-shared.typography.body size="sm" class="text-blue-1">{{ $name }}</x-shared.typography.body>
             <div class="flex w-full justify-between items-end gap-xs">
@@ -60,25 +72,9 @@
 
     @if($senderType === 'user')
     <div class="flex items-start">
+        <!-- Display user's avatar -->
         <x-shared.fragments.useravatar src="{{ $avatarSrc }}" size="sm" />
     </div>
     @endif
 </div>
 @endif
-
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll("[id^='overlay_']").forEach((overlay) => {
-        overlay.addEventListener("click", function() {
-            const index = overlay.id.split("_")[1];
-            const video = document.getElementById("video_" + index);
-            
-            if (video) {
-                overlay.style.display = "none";
-                video.classList.remove("hidden");
-                video.play();
-            }
-        });
-    });
-});
-</script>
