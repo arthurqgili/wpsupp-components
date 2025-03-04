04245417cae778c3938cb9a2e51f90c993f17cb4
Pedro Tebexreni <pedro.tebexreni@estudiono.com>
2025-03-04 15:59:30 -0300

    Added support for audio messages and individualized message rendering.

--- fragments/chatspeech.blade.php
+++ fragments/chatspeech.blade.php
@@ -1,75 +1,76 @@
 @props([
-    'avatarSrc',        // URL of the avatar image
-    'name',              // Name of the user
-    'messages' => [],    // Array of messages to display
-    'senderType',        // Type of sender (user or client)
+    'avatarSrc',        // URL do avatar
+    'name',             // Nome do usuário
+    'messages' => [],   // Array de mensagens
+    'senderType',       // Tipo do remetente ('user' ou 'client')
 ])
 
 @if($messages)
-<div class="flex gap-xxxs {{ $senderType === 'user' ? 'justify-end' : '' }}">
-    @if($senderType === 'client')
-    <div class="flex items-start">
-        <!-- Display client's avatar -->
-        <x-shared.fragments.useravatar src="{{ $avatarSrc }}" size="xs" />
-    </div>
-    @endif
-
-    <div class="flex flex-col gap-xxxs {{ $senderType === 'user' ? 'items-end' : '' }} max-w-full">
+    <div class="flex flex-col gap-xxxs {{ $senderType === 'user' ? 'justify-end' : '' }}">
         @foreach($messages as $message)
-        @php
-        $formattedTime = \Carbon\Carbon::parse($message['sentAt'])->format('H:i');
-        @endphp
+            @php
+                $formattedTime = \Carbon\Carbon::parse($message['sentAt'])->format('H:i');
+            @endphp
 
-        @if (isset($message['has_media']) && $message['has_media'])
-            @if (strpos($message['media_url'], '.mp4') !== false)
-            <!-- Video message with Alpine.js control -->
-            <div x-data="{ showVideo: false }" class="w-full max-w-[240px] min-h-xxxl relative">
-                <!-- Video player, shown when video is clicked -->
-                <video x-show="showVideo" class="w-full h-full object-cover rounded-xxs" controls>
-                    <source src="{{ $message['media_url'] }}" type="video/mp4">
-                </video>
-                <!-- Play button overlay when video is not shown -->
-                <div x-show="!showVideo" @click="showVideo = true" class="absolute inset-0 flex items-center justify-center bg-black/50 cursor-pointer rounded-xxs">
-                    <span class="material-symbols-outlined text-white text-6xl">play_arrow</span>
+            <div class="flex gap-xxxs {{ $senderType === 'user' ? 'justify-end' : '' }}">
+
+                <!-- Se o remetente for o 'client', exibe o avatar -->
+                @if($senderType === 'client')
+                    <div class="flex items-start">
+                        <x-shared.fragments.useravatar src="{{ $avatarSrc }}" size="xs" />
+                    </div>
+                @endif
+
+                <!-- Bloco de mensagens -->
+                <div class="flex flex-col gap-xxxs {{ $senderType === 'user' ? 'items-end' : '' }} max-w-full">
+                    
+                    <!-- Verifica se a mensagem possui mídia -->
+                    @if(isset($message['has_media']) && $message['has_media'])
+                        @if(strpos($message['media_url'], '.mp4') !== false)
+                            <!-- Mensagem de vídeo -->
+                            <div class="w-full max-w-[240px] min-h-xxxl relative">
+                                <video x-show="showVideo" class="w-full h-full object-cover rounded-xxs" controls>
+                                    <source src="{{ $message['media_url'] }}" type="video/mp4">
+                                </video>
+
+                            </div>
+                        @elseif(strpos($message['media_url'], '.mp3') !== false)
+                            <!-- Mensagem de áudio -->
+                            <x-shared.fragments.chataudio :avatarSrc="$avatarSrc" :name="$name" :audioSrc="$message['media_url']" :sentAt="$message['sentAt']" :senderType="$senderType" />
+                        @else
+                            <!-- Imagem -->
+                            <div class="w-full max-w-[240px] min-h-xxxl relative">
+                                <img class="w-full h-full object-cover rounded-xxs" src="{{ $message['media_url'] }}" alt="Imagem da mensagem">
+                                <div class="absolute bottom-0 right-0 p-xs">
+                                    <x-shared.typography.body size="sm" class="text-gray-3">{{ $formattedTime }}</x-shared.typography.body>
+                                </div>
+                            </div>
+                        @endif
+                    @elseif ($message['content'])
+                        <!-- Mensagem de texto -->
+                        <div class="max-w-[240px] w-fit bg-black-1 flex flex-col p-xs rounded-xxs">
+                            <x-shared.typography.body size="sm" class="text-blue-1">{{ $name }}</x-shared.typography.body>
+                            <div class="flex w-full justify-between items-end gap-xs">
+                                <div class="flex-1 min-w-0 break-words">
+                                    <x-shared.typography.body size="md" class="text-white">
+                                        {{ $message['content'] }}
+                                    </x-shared.typography.body>
+                                </div>
+                                <div class="whitespace-nowrap">
+                                    <x-shared.typography.body size="sm" class="text-gray-3">{{ $formattedTime }}</x-shared.typography.body>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
                 </div>
-                <div class="absolute bottom-0 right-0 p-xs">
-                    <x-shared.typography.body size="sm" class="text-gray-3">{{ $formattedTime }}</x-shared.typography.body>
-                </div>
-            </div>
-            @else
-            <!-- Image message (No lightbox, just the image) -->
-            <div class="w-full max-w-[240px] min-h-xxxl relative">
-                <!-- Image thumbnail -->
-                <img class="w-full h-full object-cover rounded-xxs" src="{{ $message['media_url'] }}" alt="Message image">
-                <div class="absolute bottom-0 right-0 p-xs">
-                    <x-shared.typography.body size="sm" class="text-gray-3">{{ $formattedTime }}</x-shared.typography.body>
-                </div>
-            </div>
-            @endif
-        @elseif ($message['content'])
-        <!-- Text message -->
-        <div class="max-w-[240px] w-fit bg-black-1 flex flex-col p-xs rounded-xxs">
-            <x-shared.typography.body size="sm" class="text-blue-1">{{ $name }}</x-shared.typography.body>
-            <div class="flex w-full justify-between items-end gap-xs">
-                <div class="flex-1 min-w-0 break-words">
-                    <x-shared.typography.body size="md" class="text-white">
-                        {{ $message['content'] }}
-                    </x-shared.typography.body>
-                </div>
-                <div class="whitespace-nowrap">
-                    <x-shared.typography.body size="sm" class="text-gray-3">{{ $formattedTime }}</x-shared.typography.body>
-                </div>
-            </div>
-        </div>
-        @endif
+
+                <!-- Se o remetente for o 'user', exibe o avatar -->
+                @if($senderType === 'user')
+                    <div class="flex items-start">
+                        <x-shared.fragments.useravatar src="{{ $avatarSrc }}" size="xs" />
+                    </div>
+                @endif
+            </div> <!-- Fim do container da mensagem -->
         @endforeach
     </div>
-
-    @if($senderType === 'user')
-    <div class="flex items-start">
-        <!-- Display user's avatar -->
-        <x-shared.fragments.useravatar src="{{ $avatarSrc }}" size="xs" />
-    </div>
-    @endif
-</div>
 @endif
